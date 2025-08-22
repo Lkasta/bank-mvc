167ab0c91b0ef9b11d54d88fd788535cd86751a1
Lkasta <lkastabackup@gmail.com>
2025-08-22 15:57:41 -0300

    feat: implement company entities

---
+++ src/models/sqlite/entities/company.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, String, REAL, BIGINT
+from src.models.sqlite.settings.base import Base
+
+class CompanyTable(Base):
+  __tablename__ = 'company'
+
+  id = Column(BIGINT, primary_key=True)
+  monthly_income = Column(REAL, primary_key=False)
+  age = Column(BIGINT, primary_key=False)
+  full_name = Column(String, primary_key=False)
+  phone = Column(String, primary_key=False)
+  email = Column(String, primary_key=False)
+  category = Column(String, primary_key=False)
+  balance = Column(REAL, primary_key=False)
+
+  def __repr__(self):
+    return (
+      f"Indivisuals ["
+      f"full_name={self.full_name}, "
+      f"age={self.age}, "
+      f"phone={self.phone}, "
+      f"email={self.email}, "
+      f"category={self.category}, "
+      f"monthly_income={self.monthly_income}, "
+      f"balance={self.balance}"
+      f"]"
+    )
